52041c90d10e96e6203859442f834ebf66c3e59c
Ben Werdmuller <ben@benwerd.com>
2021-04-27 23:06:26 -0700

    Improved ID use

--- Main.php
+++ Main.php
@@ -9,15 +9,15 @@ namespace IdnoPlugins\Event {
 
             // Events
             \Idno\Core\Idno::site()->routes()->addRoute('/event/edit/?', '\IdnoPlugins\Event\Pages\Edit');
-            \Idno\Core\Idno::site()->routes()->addRoute('/event/edit/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Event\Pages\Edit');
-            \Idno\Core\Idno::site()->routes()->addRoute('/event/delete/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Event\Pages\Delete');
-            \Idno\Core\Idno::site()->routes()->addRoute('/event/([A-Za-z0-9\-]+)/.*', '\Idno\Pages\Entity\View');
+            \Idno\Core\Idno::site()->routes()->addRoute('/event/edit/:id/?', '\IdnoPlugins\Event\Pages\Edit');
+            \Idno\Core\Idno::site()->routes()->addRoute('/event/delete/:id/?', '\IdnoPlugins\Event\Pages\Delete');
+            \Idno\Core\Idno::site()->routes()->addRoute('/event/:id/.*', '\Idno\Pages\Entity\View');
 
             // RSVPs
             \Idno\Core\Idno::site()->routes()->addRoute('/rsvp/edit/?', '\IdnoPlugins\Event\Pages\RSVP\Edit');
-            \Idno\Core\Idno::site()->routes()->addRoute('/rsvp/edit/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Event\Pages\RSVP\Edit');
-            \Idno\Core\Idno::site()->routes()->addRoute('/rsvp/delete/([A-Za-z0-9\-]+)/?', '\IdnoPlugins\Event\Pages\RSVP\Delete');
-            \Idno\Core\Idno::site()->routes()->addRoute('/rsvp/([A-Za-z0-9\-]+)/.*', '\Idno\Pages\Entity\View');
+            \Idno\Core\Idno::site()->routes()->addRoute('/rsvp/edit/:id/?', '\IdnoPlugins\Event\Pages\RSVP\Edit');
+            \Idno\Core\Idno::site()->routes()->addRoute('/rsvp/delete/:id/?', '\IdnoPlugins\Event\Pages\RSVP\Delete');
+            \Idno\Core\Idno::site()->routes()->addRoute('/rsvp/:id/.*', '\Idno\Pages\Entity\View');
 
         }
 
